f75b651f827f97949a4fb6aa4a5d29546963c1af
dashingshakeel <shkeelrauf@gmail.com>
2016-09-25 11:51:22 +0500

    Added bid model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :products do
-    resources :auctions,only: [:create]
+    resources :auctions,only: [:create] do
+      resources :bids, only: [:create]
+    end
   end
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
